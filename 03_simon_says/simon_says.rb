a15eeb3dab6f14011e86f575d3f781591a684115
Elena Kashtanova <lenachestnut@gmail.com>
2020-03-25 14:06:55 +0300

    Add solution to problem 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,40 @@
 #write your code here
+def echo string
+    string
+end
+
+def shout string
+    string.upcase
+end
+
+def repeat string, times = 2
+    ([string]*times).join(" ")
+end
+
+def start_of_word string, n
+
+    # use a range
+    string[0...n]
+    
+end
+
+def first_word string
+
+    string.split(" ").first
+
+end
+
+def titleize string
+    little_words = ["the", "and", "over"]
+
+    array = string.split(" ").map do |word|
+        if little_words.include?(word)
+            word
+        else
+            word.capitalize
+        end
+    end
+
+    array.first.capitalize!
+    array.join(" ")
+end
