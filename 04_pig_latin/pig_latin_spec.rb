4f910d1c43a4b392f7a547ad8b7ee2b2b110b1df
Elena Kashtanova <lenachestnut@gmail.com>
2020-03-25 14:08:14 +0300

    Add bonus tests

--- 04_pig_latin/pig_latin_spec.rb
+++ 04_pig_latin/pig_latin_spec.rb
@@ -68,5 +68,14 @@ describe "#translate" do
   # Test-driving bonus:
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
   # * retain the punctuation from the original phrase
+  it "keeps capitalized words" do
+    s = translate("Hello World")
+    expect(s).to eq("Ellohay Orldway")
+  end
+
+  it "retain the punctuation" do
+    s = translate("Hello, World!")
+    expect(s).to eq("Ellohay, Orldway!")
+  end
 
 end
