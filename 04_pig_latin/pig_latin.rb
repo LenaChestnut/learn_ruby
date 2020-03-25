94c3490801be80a22c7d5e3b881441f1732c57c2
Elena Kashtanova <lenachestnut@gmail.com>
2020-03-25 14:08:55 +0300

    Add solution to problem 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,44 @@
 #write your code here
+def translate(string)
+
+    sentence = string.split(" ").map do |word|
+        $end = "ay"
+
+        isCapitalized = false
+
+        if word[0] == word[0].capitalize
+            isCapitalized = true
+        end
+
+        word = word.downcase
+
+        if word.match(/\A[aeiouy]/i)
+            word = word + $end
+        elsif word.match(/\A[^aeiouy]+/i)
+            # check if the word has "qu"
+            if word.match(/\A[^aeiouy]*qu[^aeiouy]*/i)
+                match = word.match(/\A[^aeiouy]*qu[^aeiouy]*/i).to_s
+                $end = match + $end
+                word = word.sub(/\A[^aeiouy]*qu[^aeiouy]*/i, "") + $end
+            else
+                match = word.match(/\A[^aeiouy]+/i).to_s
+                $end = match + $end
+                word = word.sub(/\A[^aeiouy]+/i, "") + $end
+            end
+        end
+
+        if word.match(/[[:punct:]]/)
+            punctuation = word.match(/[[:punct:]]/).to_s
+            word = word.sub(/[[:punct:]]/, "") + punctuation
+        end
+
+        if isCapitalized == true
+            word = word.capitalize
+        end
+
+        word
+
+    end
+    
+    sentence.join(" ")
+end
